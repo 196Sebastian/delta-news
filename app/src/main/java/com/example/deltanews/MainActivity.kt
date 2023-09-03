b8f6d60a24c35fb286aee2b23e42bab441bd017c
196Sebastian <correa.sebas6@gmail.com>
2023-09-03 14:39:01 +0900

    Firebase Realtime Database: Project Creation

--- app/src/main/java/com/example/deltanews/MainActivity.kt
+++ app/src/main/java/com/example/deltanews/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.deltanews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.EventListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        myRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.i("SNAPSHOT", snapshot.value.toString())
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                // Noting to do
+            }
 
-
+        })
     }
 }
