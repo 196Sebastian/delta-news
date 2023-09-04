4c69730eba050a5652ac21e1d3a848736e1280a6
196Sebastian <correa.sebas6@gmail.com>
2023-09-04 15:13:29 +0900

    MVVM setup

--- app/src/main/java/com/example/deltanews/NewsFeedActivity.kt
+++ app/src/main/java/com/example/deltanews/NewsFeedActivity.kt
@@ -3,17 +3,20 @@ package com.example.deltanews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.deltanews.databinding.ActivityNewsFeedBinding
 
 class NewsFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityNewsFeedBinding =
-            DataBindingUtil.setContentView(this@NewsFeedActivity, R.layout.activity_news_feed)
+            DataBindingUtil.setContentView(this, R.layout.activity_news_feed)
 
-        binding.textOnScreen = "Hello"
-        binding.textView.postDelayed({
-            binding.textOnScreen = "Hello Again"
-        }, 2_000)
+        val viewModel: NewsFeedViewModel = ViewModelProvider(this)[NewsFeedViewModel::class.java]
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.fetchNewsFeed()
     }
 }
