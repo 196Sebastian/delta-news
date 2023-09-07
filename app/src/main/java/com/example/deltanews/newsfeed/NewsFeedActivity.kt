8087f8d5e6cb6305f0a7ea467a8b26568b7dee70
196Sebastian <correa.sebas6@gmail.com>
2023-09-07 16:15:11 +0900

    Removed duplicated code

--- app/src/main/java/com/example/deltanews/newsfeed/NewsFeedActivity.kt
+++ app/src/main/java/com/example/deltanews/newsfeed/NewsFeedActivity.kt
@@ -2,13 +2,10 @@ package com.example.deltanews.newsfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.example.deltanews.R
 import com.example.deltanews.databinding.ActivityNewsFeedBinding
-import com.example.deltanews.model.NewFeedItem
 
 class NewsFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +23,4 @@ class NewsFeedActivity : AppCompatActivity() {
 
         viewModel.fetchNewsFeed()
     }
-}
-
-@BindingAdapter("setItems")
-fun setItem(recyclerView: RecyclerView, list: List<NewFeedItem>?) {
-    (recyclerView.adapter as NewsFeedRecyclerViewAdapter).setNewsFeedItem(list)
-
 }
